9105186a08dd2e6de2f58ec6823b6a99cf709442
Jefry-P <00016623@uca.edu.sv>
2024-09-23 10:45:20 -0600

    feat: edit function, color by types css

--- script/index.js
+++ script/index.js
@@ -3,8 +3,8 @@ const baseURL = "https://pokeapi.co/api/v2/pokemon";
 const errorLabel = document.querySelector('.error');
 const list = document.querySelector(".pokemon-list");
 const form = document.querySelector('#search-form')
-const isEditing = false;
-const currentEditingItem = null;
+let isEditing = false;
+let currentEditingItem = null;
 
 async function searchPokemon(paramName) {
     try {
@@ -16,6 +16,8 @@ async function searchPokemon(paramName) {
 
         const cardSection = document.createElement('div');
         cardSection.classList.add('card-section');
+        const bar = document.createElement('div');
+        bar.classList.add('bar');
         const img = document.createElement('img');
 
         const infoSection = document.createElement('div');
@@ -30,9 +32,12 @@ async function searchPokemon(paramName) {
         id.innerText = `#${data.id}`;
         name.innerText = data.name;
 
-        data.types.forEach(element => {
+        data.types.forEach((element, index) => {
             const liType = document.createElement('li');
             liType.innerText = element.type.name;
+            liType.classList.add('type');
+            liType.classList.add(element.type.name);
+            if(index===0) bar.classList.add(element.type.name);
             types.appendChild(liType);
         });
 
@@ -40,6 +45,7 @@ async function searchPokemon(paramName) {
         infoSection.appendChild(name);
         infoSection.appendChild(types);
 
+        cardSection.appendChild(bar);
         cardSection.appendChild(img);
         cardSection.appendChild(infoSection);
         
@@ -49,9 +55,11 @@ async function searchPokemon(paramName) {
 
         const updateButton = document.createElement('button');
         updateButton.innerText = "Edit";
-
         updateButton.classList.add('btn');
-        updateButton.classList.add("update"); 
+        updateButton.classList.add("update");
+        updateButton.addEventListener("click", () => editMode(li, name));
+
+        
 
         const deleteButton = document.createElement('button');
         deleteButton.innerText = 'Delete';
@@ -64,7 +72,12 @@ async function searchPokemon(paramName) {
         
         li.appendChild(cardSection);
         li.appendChild(buttons);
-        list.appendChild(li);
+        if(isEditing) {
+          currentEditingItem.li.replaceWith(li);
+          resetForm();
+        } 
+        else list.appendChild(li);
+        
     } catch (error) {
         console.log(error);
         errorLabel.innerText = "No se encontró ningún Pokémon con ese nombre";
@@ -73,26 +86,21 @@ async function searchPokemon(paramName) {
     }
 }
 
-
 const editMode = (li, span) => {
   document.getElementById("name").value = span.innerText;
   isEditing = true;
   currentEditingItem = { li, span };
-  attendantForm.querySelector('input[type="submit"]').value =
-    "Update Attendant";
+  form.querySelector('input[type="submit"]').value =
+    "Find and replace";
 };
 
-// const updatePokemon = (name) => {
-//   currentEditingItem.span.innerText = name;
-//   resetForm();
-// };
 
-// const resetForm = () => {
-//   document.getElementById("name").value = "";
-//   isEditing = false;
-//   currentEditingItem = null;
-//   attendantForm.querySelector('input[type="submit"]').value = "Add Attendant";
-// };
+const resetForm = () => {
+  document.getElementById("name").value = "";
+  isEditing = false;
+  currentEditingItem = null;
+  form.querySelector('input[type="submit"]').value = "Search";
+};
 
 const deletePokemon = (li) => {
   if (confirm("Are you sure you want to delete this Pokémon?")) {
@@ -107,11 +115,7 @@ form.addEventListener('submit', (event) =>{
     const name = inputs['name'];
 
     if (name.value.trim() !== "") {
-      if (isEditing) {
-        //updatePokemon(name.value.toLowerCase());
-      } else {
-        searchPokemon(name.value.toLowerCase());
-      }
+      searchPokemon(name.value.toLowerCase());
       name.value = "";
     } else {
       errorLabel.innerText = 'Ingrese el nombre de un Pokémon';
